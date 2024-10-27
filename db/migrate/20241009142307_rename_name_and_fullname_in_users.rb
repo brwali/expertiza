af43867e6751201ba387420ed172e69c7e62e988
Manav Patel <mhpatel4@ncsu.edu>
2024-10-27 17:58:09 -0400

    update migration for fullname to name

--- db/migrate/20241009142307_rename_name_and_fullname_in_users.rb
+++ db/migrate/20241009142307_rename_name_and_fullname_in_users.rb
@@ -2,5 +2,6 @@ class RenameNameAndFullnameInUsers < ActiveRecord::Migration[5.1]
   def change
     rename_column :users, :name, :username
     rename_column :account_requests, :name, :username
+    rename_column :users, :fullname, :name
   end
 end
